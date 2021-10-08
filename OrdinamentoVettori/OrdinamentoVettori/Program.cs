7ab10e284915a8f89be3d29983e7b7f583bd3738
Abderazak Erraknaoui <ABDERAZAK.ERRAKNAOUI@local.ittcesena.it>
2021-10-08 08:35:22 +0200

    Aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,11 +6,12 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //Creazione vettore 
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //Richiamo il metodo ordina
             ordina(vettore);
-
+            //Stampo a video l'array
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
@@ -19,6 +20,7 @@ namespace OrdinamentoVettori
 
         static void ordina (int[] vettore)
         {
+            //metodo per l'ordinamento dell'array
             Array.Sort(vettore);
         }
 
